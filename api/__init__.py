f96b52e3f4479ee413e31e48b941c3f7dced9f3a
alexis.baladon <alexis.baladon@fing.edu.uy>
2023-10-13 17:09:39 -0300

    [FEATURE] Index design

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_socketio import SocketIO
 
-app = Flask(__name__, template_folder='modules')
+app = Flask(__name__, 
+            template_folder='modules', 
+            static_url_path='/static')
 app.config['SECRET_KEY'] = 'secret'
 socketio = SocketIO(app, async_mode="threading")
 

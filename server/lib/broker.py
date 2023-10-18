8f73aaaf55ff5b028182bb8a5a1c5c0d9d64e3ab
alexis.baladon <alexis.baladon@fing.edu.uy>
2023-10-18 11:52:32 -0300

    [FEATURE] Global context used instead of redis for user data

--- server/lib/broker.py
+++ server/lib/broker.py
@@ -2,6 +2,7 @@ import json
 
 from flask_redis import FlaskRedis
 from server import app
+from flask import g
 
 def create_broker(app=app):
     return FlaskRedis(app)
@@ -12,11 +13,18 @@ def set(key: str, value, object=True):
     if object:
         value = json.dumps(value)
 
-    broker.set(key, value)
+    #broker.set(key, value)
+    
+    with app.app_context():
+        setattr(g, key, value)
+
     return
 
 def get(key: str, object=True):
-    value = broker.get(key)
+    #value = broker.get(key)
+
+    with app.app_context():
+        value = getattr(g, key)
 
     if object and value is not None:
         value = json.loads(value)
